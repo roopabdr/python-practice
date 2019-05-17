38fc0b9f03a507b48a3ba166f5cfee2a6494f26a
roopabdr <roopabdr@gmail.com>
2019-05-17 10:39:23 +0530

    Handled additional test cases

--- MinMax.py
+++ MinMax.py
@@ -1,5 +1,19 @@
-def miniMaxSum(arr):
-	if len(arr) == 5:
-		print('{} {}'.format(sum([min_val for min_val in arr if min_val<max(arr)]), sum([max_val for max_val in arr if max_val > min(arr)])))
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
 
-miniMaxSum([1,3,5,7,9])
+# Complete the miniMaxSum function below
+def miniMaxSum(arr):
+	if len(arr) == 5 and min(arr) != max(arr):
+		print('{} {}'.format(sum([min_val for min_val in arr if min_val<max(arr)]), sum([max_val for max_val in arr if max_val > min(arr)])))
+	elif len(arr) == 5 and min(arr) == max(arr):
+		arr.pop()
+		print('{} {}'.format(sum(arr), sum(arr)))
+
+if __name__ == '__main__':
+	arr = list(map(int, input().rstrip().split()))
+	miniMaxSum(arr)
+	#miniMaxSum([5,5,5,5,5])
