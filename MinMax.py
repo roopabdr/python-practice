ea14ad79bf0180b05e33540ffec7a315bfebc339
roopabdr <roopabdr@gmail.com>
2019-05-30 08:59:58 +0530

    initiated Gaussian project

--- MinMax.py
+++ MinMax.py
@@ -8,12 +8,17 @@ import sys
 # Complete the miniMaxSum function below
 def miniMaxSum(arr):
 	if len(arr) == 5 and min(arr) != max(arr):
-		print('{} {}'.format(sum([min_val for min_val in arr if min_val<max(arr)]), sum([max_val for max_val in arr if max_val > min(arr)])))
-	elif len(arr) == 5 and min(arr) == max(arr):
-		arr.pop()
-		print('{} {}'.format(sum(arr), sum(arr)))
+		print('{} {}'
+		.format(
+			sum([min_val for i, min_val in enumerate(arr) if (min_val < max(arr) and i < len(arr)-1) or (min_val==max(arr) and i <= len(arr)-2)])
+			,sum([max_val for j, max_val in enumerate(arr) if (max_val > min(arr) and j < len(arr)-1) or (max_val==max(arr) and j <= len(arr)-2)])
+			)
+		)
+	# elif len(arr) == 5 and min(arr) == max(arr):
+	# 	arr.pop()
+	# 	print('{} {}'.format(sum(arr), sum(arr)))
 
 if __name__ == '__main__':
-	arr = list(map(int, input().rstrip().split()))
-	miniMaxSum(arr)
-	#miniMaxSum([5,5,5,5,5])
+	# arr = list(map(int, input().rstrip().split()))
+	# miniMaxSum(arr)
+	miniMaxSum([1,5,5,5,5])
